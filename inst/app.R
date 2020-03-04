d056f95516076c1ce856e4068375082548a038a5
mike <jmj00@ucsb.edu>
2020-03-03 20:43:54 -0800

    add download csv option

--- inst/app.R
+++ inst/app.R
@@ -3,59 +3,34 @@ library(leaflet)
 library(sf)
 library(ggplot2)
 library(dplyr)
-library(patchwork)
 library(shinycustomloader)
 devtools::load_all()
 
 make_plots = function(x){
   
-  history = nwmHistoric::extract_retro_url(comid = pt$comid)
+  history = nwmHistoric::extract_retro_url(x)
   
   vals = history %>%
     group_by(year, month, day) %>%
     summarise(flow = max(flow)) %>%
     ungroup() %>%
     mutate(date = as.Date(paste(year, month, day, sep = "-")))
-  
-  ann_peak = history %>% 
-    group_by(year) %>% 
-    summarise(peak = max(flow))
-  
-  
+
   p = ggplot(data = vals, aes(x = date, y = flow)) +
     geom_line(size = .1) +
     labs(x = 'Date', y = 'Streamflow (cms)') +
-    #geom_hline(yintercept = bf, col = 'darkred') +
     theme_classic()
-  
-  # p2 = ggplot(data = ann_exceed, 
-  #             aes(x = as.Date(paste0(year, "-01-01")), 
-  #                 y = value,
-  #                 group = variable, col = variable)) +
-  #   geom_col(color = "darkgray", fill = 'darkgray') + 
-  #   geom_smooth(col = 'darkred') +
-  #   ylim(c(0,max(ann_exceed$value))) +
-  #   labs(x = "Year", y = "Number of bankfull exceedances") +
-  #   
-  #   scale_x_date(labels = scales::date_format("%Y")) +
-  #   theme_classic()
-  
-  p3 = p #+ p2
-  return(p3)
+
+  return(p)
 }
-source('/Users/mikejohnson/Documents/retro_hydro/R/helpers.R')
 
-
-
-X = seq(-97.36996, -93.64555, 9.259171e-05 )
-Y = seq( 28.87556,  32.22229, 9.259171e-05)
 pt = NULL
 lat  =  29.93038
 lon  = -95.37706
 
 ui <- (
   fluidPage(
-    titlePanel('NWM v2.0 Reanalysis'),
+    titlePanel('NWM v2.0 Reanalysis: Tester'),
     tags$style(type = "text/css", "#map {height: calc(100vh - 80px) !important;}"),
     leafletOutput('map') %>% withLoader(loader="pacman")
   )
@@ -81,8 +56,8 @@ server <- function(input, output,session) {
       ) %>% 
       addTerminator(group = "daylight") %>% 
       addLayersControl(
-        baseGroups = c("Grayscale", "OSM", "Imagery"), #, "Rainfall", "Night/Day"),
-        overlayGroups = c("Rainfall", "daylight", 'risk-point'),
+        baseGroups = c("Grayscale", "OSM", "Imagery"), 
+        overlayGroups = c("Rainfall", "daylight"),
         options = layersControlOptions(collapsed = TRUE),
         position = 'topleft') %>% 
       addScaleBar("bottomleft") %>%
@@ -105,9 +80,13 @@ server <- function(input, output,session) {
   #Show popup on click
   observeEvent(input$map_click, {
     
-    click <- input$map_click %>% data.frame() %>% dplyr::select(lat,lng)
+    click <- input$map_click %>% 
+      data.frame() %>% 
+      dplyr::select(lat,lng)
   
-    pt = sf::st_as_sf(click, coords = c("lng", "lat"), crs = '+proj=longlat +datum=WGS84')
+    pt = sf::st_as_sf(click, 
+                      coords = c("lng", "lat"), 
+                      crs = '+proj=longlat +datum=WGS84')
     
     rev = AOI::geocode_rev(c(click$lat, click$lng))
     
@@ -115,7 +94,6 @@ server <- function(input, output,session) {
     pt$osm_type = rev$osm_type
     pt$name = rev$display_name
     pt$comid = nhdplusTools::discover_nhdplus_id(pt)
-    
     
     pt <<- pt 
     
@@ -126,8 +104,6 @@ server <- function(input, output,session) {
                  popup = paste(leafpop::popupTable(st_drop_geometry(pt), 
                                                    row.numbers = FALSE, 
                                                    feature.id = FALSE), "</b></br>", actionButton("selectlocation", "Plot this Reach", onclick = 'Shiny.onInputChange(\"button_click\",  Math.random())') ))
-    
-    
   })
   
   observeEvent(input$button_click, {
@@ -137,7 +113,8 @@ server <- function(input, output,session) {
       renderPlot({
         make_plots(pt$comid)
       }),
-      downloadButton('foo'),
+      downloadButton('foo', label = "Download PNG"),
+      downloadButton('foo2', label = "Download CSV"),
       size = "l", easyClose = TRUE
     ))
   })
@@ -151,6 +128,13 @@ server <- function(input, output,session) {
       }
       ggsave(file, plot = last_plot(), device = device)
     })
+  
+  output$foo2 = downloadHandler(
+    filename = paste0('historic_nwm_comid_', pt$comid,'.csv'),
+    content = function(file) {
+      write.csv(history, file)
+    }
+  )
   
 }
 
